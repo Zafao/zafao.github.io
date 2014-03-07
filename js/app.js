d58f90260dadb5ae4bc6256becad4724038bbe77
Thiago M Rocha <kimo@kimo.io>
2014-03-07 09:04:33 -0300

    Adding smooth scrolll

--- js/app.js
+++ js/app.js
@@ -7,4 +7,13 @@ $(function() {
       $("header").addClass("scrolled");
     }
   });
+
+  $(".menu-items li a").click(function(event) {
+    event.preventDefault();
+  });
+
+  ($("header")).onePageNav({
+    changeHash: true,
+    currentClass: 'active'
+  });
 });
